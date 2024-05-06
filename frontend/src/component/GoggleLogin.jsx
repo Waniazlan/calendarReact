e4052ab10ab9442a73680fe2e7e21346df0b41ce
Waniazlan <117734670+Waniazlan@users.noreply.github.com>
2024-05-06 15:24:41 +0800

    new commit

--- frontend/src/component/GoggleLogin.jsx
+++ frontend/src/component/GoggleLogin.jsx
@@ -12,13 +12,15 @@ const GoggleLogin = () => {
 
 
   return (
-    <div className='absolute right-[45px] mt-[160px]'>
+    <div className='absolute right-[45px] mt-[25px]'>
     {isLogging ? 
       <button className='text-white text-sm font-semibold hover:bg-slate-200 hover:text-black ease-in duration-300  rounded-xl px-2 py-2 flex gap-3 ' onClick={() => logOut()}>
+        Sign Out
         <img className='w-5 h-' src={google} alt="Google logo" />
       </button> 
       : 
       <button className='text-white text-sm font-semibold hover:bg-slate-200 hover:text-black ease-in duration-300 rounded-xl px-2 py-2 flex gap-3 ' onClick={() => login()}>
+       Sign In
         <img className='w-5 h-5' src={google} alt="Google logo" />
       </button>
     }
